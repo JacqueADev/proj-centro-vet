05f9ac8fae22564d41ca3a43ba6cdaf38f95e820
JacqueADev <jacquelineadev@gmail.com>
2025-04-08 00:10:59 -0300

    Criação das pags anamnese e novo cadastro

--- js/pos-login.js
+++ js/pos-login.js
@@ -9,12 +9,21 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = "tela-agenda.html";
     });
 
-    // Redirecionamento para Cadastrados (NOVO)
+    // Redirecionamento para Cadastrados
     const botaoCadastrados = document.getElementById("botao-cadastrados");
     if (botaoCadastrados) {
         botaoCadastrados.addEventListener("click", function() {
             console.log("Redirecionando para cadastrados...");
             window.location.href = "tela-cadastrados.html";
+        });
+    }
+
+    // Redirecionamento para Novo Atendimento (Anamnese)
+    const botaoNovoAtendimento = document.getElementById("botao-novo-atendimento");
+    if (botaoNovoAtendimento) {
+        botaoNovoAtendimento.addEventListener("click", function() {
+            console.log("Redirecionando para anamnese...");
+            window.location.href = "tela-anamnese.html";
         });
     }
 });
